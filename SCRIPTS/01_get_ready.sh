44de819fa80bf2f4c00e52fd236e83baa40c8a8e
Zhuohui Yu <agassi21@gmail.com>
2020-05-21 17:14:27 +0800

    squashfs

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -5,7 +5,4 @@ patch -p1 < ../PATCH/R2S.patch
 cd target/linux/rockchip
 mkdir patches-5.4
 cp -rf ../../../../PATCH/000-add-nanopi-r2s-support.patch patches-5.4/000-add-nanopi-r2s-support.patch
-cd ..
-cd ..
-cd ..
-exit 0
+cd ../../..
