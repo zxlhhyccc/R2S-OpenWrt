ab2aa4679729ad832105b8674a2804685f8fdf3b
Zhuohui Yu <agassi21@gmail.com>
2020-05-22 19:57:51 +0800

    get ready update

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -2,7 +2,6 @@
 git clone https://git.openwrt.org/openwrt/openwrt.git
 cd openwrt
 patch -p1 < ../PATCH/R2S.patch
-cd target/linux/rockchip
-mkdir patches-5.4
-cp -rf ../../../../PATCH/000-add-nanopi-r2s-support.patch patches-5.4/000-add-nanopi-r2s-support.patch
-cd ../../..
+mkdir target/linux/rockchip/patches-5.4
+cp -rf ../PATCH/000-add-nanopi-r2s-support.patch target/linux/rockchip/patches-5.4/000-add-nanopi-r2s-support.patch
+
