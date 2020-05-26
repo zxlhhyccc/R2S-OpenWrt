f1750d24ebfc22caa92bdd484443a1faf25fbb63
Zhuohui Yu <agassi21@gmail.com>
2020-05-26 18:26:05 +0800

    001-add_propper_rootfs_and_fstab_discovery_on_a_block_device_partitions.patch

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -4,4 +4,5 @@ cd openwrt
 patch -p1 < ../PATCH/R2S.patch
 mkdir target/linux/rockchip/patches-5.4
 cp -rf ../PATCH/000-add-nanopi-r2s-support.patch target/linux/rockchip/patches-5.4/000-add-nanopi-r2s-support.patch
-
+mkdir -p package/system/fstools/patches
+cp ../PATCH/001-add_propper_rootfs_and_fstab_discovery_on_a_block_device_partitions.patch package/system/fstools/patches
